8a832614af06587b59ff7f26e6c61fc27a219034
yahoo0122 <131500288+yahoo0122@users.noreply.github.com>
2023-06-01 12:57:46 +0000

    6.1.

--- 10950.py
+++ 10950.py
@@ -1,7 +1,4 @@
-n = int(input())
-
-for _ in range(n):
-    a, b = input().split()
-    a = int(a)
-    b = int(b)
-    print(a+b)
+a = int(input())
+for i in range(a):
+    b, c = map(int, input().split())
+    print(b+c)
